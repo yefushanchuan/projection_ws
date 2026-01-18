c0a59c0e1d6d57cb8d9cb4ba626304e302e8d957
zx <1158768027@qq.com>
2026-01-18 18:02:58 +0800

    fix

--- src/image_viewer/src/image_viewer_talker.cpp
+++ src/image_viewer/src/image_viewer_talker.cpp
@@ -2,6 +2,7 @@
 #include <memory>
 #include <string>
 #include <opencv2/opencv.hpp>
+#include <cstdlib> 
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
@@ -86,12 +87,23 @@ private:
             window_initialized_ = true;
         }
         
-        // 【关键修复】不要在这里调用 getWindowProperty 检查窗口是否可见！
-        // 在 HDMI/X11 负载高时，查询窗口属性会阻塞主线程导致卡死。
-        // 直接无脑 imshow，就算窗口被遮挡，显存也会刷新，不会死锁。
-        
         cv::imshow(window_name_, image);
         cv::waitKey(1); // 必须调用，且只等 1ms
+
+        if (wmctrl_counter_ < 10) {
+            // 构造命令字符串
+            // -r : 指定窗口名称 (必须和 cv::namedWindow 的名字完全一致)
+            // -b add,fullscreen,above : 添加 "全屏" 和 "置顶" 属性
+            std::string cmd = "wmctrl -r '" + window_name_ + "' -b add,above";
+            
+            int ret = std::system(cmd.c_str());
+            
+            // 只有当 wmctrl 成功找到窗口并执行后，计数器才增加
+            // 这样可以防止窗口还没弹出来时计数器就跑完了
+            if (ret == 0) {
+                wmctrl_counter_++;
+            }
+        }
     }
 
     // 4. 发布图像
@@ -114,6 +126,7 @@ private:
   
   std::string window_name_;
   bool window_initialized_;
+  int wmctrl_counter_ = 0; 
 };
 
 int main(int argc, char * argv[])
